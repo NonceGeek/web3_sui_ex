6528220fe256bc99d841c361514bc3f33583b29a
leeduckgo <albertschr@163.com>
2022-08-12 12:28:57 +0800

    update TypeTranslator

--- lib/type_translator.ex
+++ lib/type_translator.ex
@@ -2,4 +2,17 @@ defmodule Web3MoveEx.TypeTranslator do
   def hex_to_starcoin_byte(hex_str) do
     "x\"#{hex_str}\""
   end
+
+  def parse_type_in_move(%{U64: value}) do
+    String.to_integer(value)
+  end
+
+  def parse_type_in_move(%{Bytes: bytes}) do
+    bytes
+    |> Binary.drop(2)
+    |> Base.decode16!(case: :lower)
+  end
+
+  # TODO: impl other types
+
 end
--- mix.exs
+++ mix.exs
@@ -37,7 +37,8 @@ defmodule Web3MoveEx.MixProject do
       {:mox, "~> 1.0", only: :test},
       {:ex_struct_translator, "~> 0.1.1"},
       {:ex_doc, ">= 0.0.0", only: :dev},
-      {:earmark, ">= 0.0.0", only: :dev}
+      {:earmark, ">= 0.0.0", only: :dev},
+      {:binary, "~> 0.0.5"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
